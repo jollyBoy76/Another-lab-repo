9aebd0e21a006b572f072fa648b25c6a29b54c48
jollyBoy76 <mrdeath376@gmail.com>
2026-06-20 13:47:36 -0400

    Added Terniary OPT in controlFlow.js

--- controlFlow/controlFlow.js
+++ controlFlow/controlFlow.js
@@ -48,3 +48,11 @@ switch (userType) {
 }
 
 console.log("User Category:", userCategory);
+
+
+
+let isAuthenticated = true;
+
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+
+console.log("Authentication Status:", authenticationStatus);
